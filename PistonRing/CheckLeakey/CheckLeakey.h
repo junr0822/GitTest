605ec1e994d72394309b4cb6afea706237fc444a
junr0822 <junr0822@163.com>
2016-03-14 17:08:22 +0800

    项目创建

---
+++ PistonRing/CheckLeakey/CheckLeakey.h
@@ -0,0 +1,32 @@
+
+// CheckLeakey.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CCheckLeakeyApp: 
+// 有关此类的实现，请参阅 CheckLeakey.cpp
+//
+
+class CCheckLeakeyApp : public CWinApp
+{
+public:
+	CCheckLeakeyApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCheckLeakeyApp theApp;
